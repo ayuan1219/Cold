d87549fa2e5bd15a1c09e1eef478d329c767a0ae
Ayuan <838478249@qq.com>
2021-01-19 11:10:24 +0800

    库存管理
    Signed-off-by: Ayuan <838478249@qq.com>

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysProductServiceImpl.java
@@ -0,0 +1,90 @@
+package com.ruoyi.system.service.impl;
+
+import com.ruoyi.common.core.domain.entity.SysProduct;
+import com.ruoyi.common.utils.DateUtils;
+import com.ruoyi.system.mapper.SysProductMapper;
+import com.ruoyi.system.service.ISysProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 产品Service业务层处理
+ *
+ * @author EasyArch_Ayuan
+ * @date 2021-01-19
+ */
+@Service
+public class SysProductServiceImpl implements ISysProductService {
+    @Autowired
+    private SysProductMapper sysProductMapper;
+
+    /**
+     * 查询产品
+     *
+     * @param productId 产品ID
+     * @return 产品
+     */
+    @Override
+    public SysProduct selectSysProductById(Long productId) {
+        return sysProductMapper.selectSysProductById(productId);
+    }
+
+    /**
+     * 查询产品列表
+     *
+     * @param sysProduct 产品
+     * @return 产品
+     */
+    @Override
+    public List<SysProduct> selectSysProductList(SysProduct sysProduct) {
+        return sysProductMapper.selectSysProductList(sysProduct);
+    }
+
+    /**
+     * 新增产品
+     *
+     * @param sysProduct 产品
+     * @return 结果
+     */
+    @Override
+    public int insertSysProduct(SysProduct sysProduct) {
+        sysProduct.setCreateTime(DateUtils.getNowDate());
+        return sysProductMapper.insertSysProduct(sysProduct);
+    }
+
+    /**
+     * 修改产品
+     *
+     * @param sysProduct 产品
+     * @return 结果
+     */
+    @Override
+    public int updateSysProduct(SysProduct sysProduct) {
+        sysProduct.setUpdateTime(DateUtils.getNowDate());
+        return sysProductMapper.updateSysProduct(sysProduct);
+    }
+
+    /**
+     * 批量删除产品
+     *
+     * @param productIds 需要删除的产品ID
+     * @return 结果
+     */
+    @Override
+    public int deleteSysProductByIds(Long[] productIds) {
+        return sysProductMapper.deleteSysProductByIds(productIds);
+    }
+
+    /**
+     * 删除产品信息
+     *
+     * @param productId 产品ID
+     * @return 结果
+     */
+    @Override
+    public int deleteSysProductById(Long productId) {
+        return sysProductMapper.deleteSysProductById(productId);
+    }
+}
